77f1507b16210bf7695f40cf3200fa40f3afb5fe
Mathias Köppchen - Protronic GmbH <m.koeppchen@protronic-gmbh.de>
2025-10-01 09:59:25 +0000

    Published by publish-to-git c6e2805f5ba9ac4a9057ca3285c144ff4b6d7f35 (HEAD -> refs/heads/master, refs/remotes/origin/master, refs/remotes/origin/HEAD) Forward as Object

---
+++ index.js
@@ -0,0 +1,129 @@
+const Promise = require('bluebird');
+const childProcess = require('child_process');
+const fs = require('fs');
+const tar = require('tar');
+const path = require('path');
+const tmp = require('tmp');
+
+Promise.promisifyAll(childProcess);
+Promise.promisifyAll(fs);
+Promise.promisifyAll(tar);
+Promise.promisifyAll(tmp);
+
+const { execFileAsync, spawn } = childProcess;
+const { unlinkAsync } = fs;
+
+tmp.setGracefulCleanup();
+
+function spawnNpmWithOutput(args, options) {
+  if(!options.verbose) {
+    return execFileAsync(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', args, options);
+  }
+
+  return new Promise((resolve, reject) => {
+    const proc = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', args, Object.assign(options, {
+      stdio: ['inherit', 'pipe', 'inherit'],
+      env: Object.assign({}, process.env, Boolean(process.stdout.isTTY) && {
+        NPM_CONFIG_COLOR: 'always'
+      })
+    }));
+    let outData = '';
+    proc.on('exit', exitCode => {
+      if(exitCode === 0) { 
+        resolve(outData);
+      }
+      reject(new Error(`npm failed with error code ${exitCode}`));
+    });
+    proc.on('error', reject);
+    proc.stdout.on('data', data => {
+      outData += data.toString('utf8');
+    });
+  });
+}
+
+async function packWithNpm({ packageSpec, targetDir, verbose }) {
+  const output = (await spawnNpmWithOutput(['pack', packageSpec], {
+    cwd: targetDir,
+    verbose
+  })).trim().split(/\n/);
+  const packedFile = output[output.length - 1];
+  const packedFileAbsolute = path.join(path.resolve(targetDir), packedFile);
+  
+  try {
+    await tar.extractAsync({
+      strip: 1,
+      cwd: targetDir,
+      file: packedFileAbsolute
+    });
+  } finally {
+    await unlinkAsync(packedFileAbsolute);
+  }
+}
+
+async function publish({tag, branch, version, push, packageSpec, packOptions}, pack = packWithNpm) {
+  if (!tag) {
+    tag = `v${version}`;
+  }
+
+  const tmpDir = await tmp.dirAsync();
+
+  const git = (...args) => execFileAsync('git', args);
+  const gitInTmpDir = (...args) => execFileAsync('git', args, {
+    cwd: tmpDir
+  });
+
+  const tmpBranch = (branch)
+    ? undefined
+    : `publish-to-git-temp-${Math.random().toString(36).substring(2,12)}`;
+
+  try {
+    if (branch) {
+      await git('worktree', 'add', '--no-checkout', tmpDir, branch);
+    } else {
+      await git('worktree', 'add', '--detach', tmpDir);
+      await gitInTmpDir('checkout', '--orphan', tmpBranch);
+      await gitInTmpDir('rm', '-rf', '.');
+    }
+
+    await pack(Object.assign({
+      packageSpec: packageSpec,
+      targetDir: tmpDir,
+    }, packOptions));
+
+    await gitInTmpDir('add', '-A');
+
+    const currentCommitMessage = (await git('log', '-n', '1', '--pretty=oneline', '--decorate=full')).trim();
+    const message = `Published by publish-to-git
+${currentCommitMessage}`;
+
+    await gitInTmpDir('commit', '-m', message);
+
+    const forceOptions = push.force ? ['-f'] : [];
+
+    await git('tag', ...forceOptions, tag, `refs/heads/${branch || tmpBranch}`);
+
+    if (push) {
+      console.warn(`Pushing to remote ${push.remote}`);
+
+      try {
+        const objectsToPush = [`refs/tags/${tag}`];
+        if (branch) objectsToPush.push(`refs/heads/${branch}`);
+        await git('push', ...forceOptions, push.remote || 'origin', ...objectsToPush);
+      } catch(err) {
+        await git('tag', '-d', tag);
+        if (branch) await gitInTmpDir('reset', '--hard', `HEAD~`);
+        throw err;
+      }
+      console.log(`Pushed tag to ${push.remote} with tag: ${tag}`);
+    } else {
+      console.log(`Created local tag: ${tag}`);
+    }
+  } finally {
+    try {
+      await git('worktree', 'remove', tmpDir);
+      if (tmpBranch) await git('branch', '-D', tmpBranch);
+    } catch(err) {}
+  }
+}
+
+module.exports = { publish, packWithNpm };
---
+++ main.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+const { publish } = require('./');
+const yargs = require('yargs');
+const argv = yargs
+  .usage('Usage: $0')
+  .example('$0 --tag v2.1.3 --no-push     # by default version from package.json is used')
+  .example('$0 --remote https://USER:GITHUB_TOKEN@github.com/USER/REPO')
+  .example('$0 --force    # useful in CI and when we want to override the same tag which triggered the build')
+  .describe('remote', 'Git remote, may be remote name or full URL to the repo')
+  .default('remote', 'origin')
+  .describe('tag', 'Tag name to which src will be published, for example: v1.2.3 - by default uses version from package.json')
+  .describe('branch', "Branch name to append this new release to - none by default")
+  .describe('push', 'Push update to the git remote (pass --no-push to disable)')
+  .describe('force', 'Override any existing tag on the remote as well as locally (git tag -f, git push -f)')
+  .parserConfiguration({ 'populate--': true })
+  .describe('--', 'All arguments after -- are passed to npm pack command')
+  .boolean('push')
+  .boolean('force')
+  .default('push', 'true')
+  .wrap(yargs.terminalWidth())
+  .argv;
+
+const path = require('path');
+const packageJson = require(path.join(process.cwd(), '/package.json'));
+
+// packageSpec is the first non-option token after `--`.
+// Remaining forwarded tokens are forwarded.
+const forwarded = yargs(argv['--'] || [])
+  .default('verbose', 'true')
+  .wrap(yargs.terminalWidth())
+  .argv;
+
+const packageSpec = forwarded._.length > 0 ? forwarded._ : process.cwd();
+console.log('Using packageSpec:', packageSpec);
+
+publish({
+  tag: argv.tag,
+  branch: argv.branch,
+  name: packageJson.name,
+  version: packageJson.version,
+  push: argv.push && {
+    remote: argv.remote,
+    force: argv.force,
+  },
+  packageSpec,
+  packOptions: {
+    ...forwarded
+  }
+}).catch(err => {
+  if (err.cmd) {
+    console.error(err.message);
+    if (err.cmd.match(/^git push/)) {
+      console.warn(`Cleaned up unpushed tag - please try again`);
+    }
+  } else {
+    console.error(err);
+  }
+  process.exit(1);
+});
